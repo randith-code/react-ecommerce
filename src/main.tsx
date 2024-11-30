5855fe2339edca5b10d3a712dd3912e0ba1d83de
randith-code <randithmadhusanka@gmail.com>
2024-12-01 00:43:10 +0530

    finish login & signup

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-// import App from "./App.tsx";
 
 import { AuthProvider } from "./contexts/AuthContext.tsx";
 import { CartProvider } from "./contexts/CartContext.tsx";
@@ -14,6 +13,5 @@ createRoot(document.getElementById("root")!).render(
         <AppRouter />
       </CartProvider>
     </AuthProvider>
-    {/* <App /> */}
   </StrictMode>
 );
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,5 +1,64 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
 const Register = () => {
-  return <div></div>;
+  return (
+    <main className="w-full h-screen grid place-items-center">
+      <Card className="w-[350px]">
+        <CardHeader>
+          <CardTitle>Sign Up</CardTitle>
+          <CardDescription>
+            Sign up to unlock seamless shopping experience.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="name">Name</Label>
+                <Input id="name" type="text" placeholder="Enter Name" />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="email">Email</Label>
+                <Input id="email" type="email" placeholder="Enter email" />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="password">Password</Label>
+                <Input
+                  id="password"
+                  type="password"
+                  placeholder="Enter Password"
+                />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="confirmpassword">Confirm Password</Label>
+                <Input
+                  id="confirmpassword"
+                  type="password"
+                  placeholder="Confirm Password"
+                />
+              </div>
+            </div>
+
+            <div className="flex justify-between pt-8">
+              <Button type="reset" variant="outline">
+                Clear
+              </Button>
+              <Button type="submit">Sign up</Button>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </main>
+  );
 };
 
 export default Register;
